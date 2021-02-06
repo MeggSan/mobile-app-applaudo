e1887c23afff84ab287fd9d275d78b401b8cab1d
meggsan <meggie.sanchez@gmail.com>
2021-02-06 19:03:52 -0400

    :heavy_plus_sign: added axios and :hammer: added base axios configuration with an animes get to test if it is working

--- src/navigators/AnimesNavigator.tsx
+++ src/navigators/AnimesNavigator.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 // SCREENS
-import {Animes} from '../screens/animes/Animes';
-import {AnimeDetail} from '../screens/animeDetail/AnimeDetail';
-import {Favorites} from '../screens/favorites/Favorites';
+import {Animes} from '@screens/animes/Animes';
+import {AnimeDetail} from '@screens/animeDetail/AnimeDetail';
+import {Favorites} from '@screens/favorites/Favorites';
 
 const Stack = createStackNavigator();
 
