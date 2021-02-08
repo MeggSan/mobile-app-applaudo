00625e8d2d468f66a962dddea268257c28c93014
meggsan <meggie.sanchez@gmail.com>
2021-02-08 16:41:53 -0400

    :art: improved some render into separated little components

---
+++ src/components/inlineInfo/InlineInfoStyles.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export const Styles = StyleSheet.create({
+  viewInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
--- src/components/list/ListStyles.ts
+++ src/components/list/ListStyles.ts
@@ -1,15 +1,13 @@
 import {StyleSheet} from 'react-native';
 
 // CONSTANTS
-import {PALETTE} from '@constants/Palette';
+import {COLORS} from '@constants/Colors';
 
 export const Styles = StyleSheet.create({
-  imageProfile: {
-    width: '100%',
-    height: 150,
-    ...PALETTE.roundedBorder,
-  },
   inlineView: {
     flexDirection: 'row',
+  },
+  marginBotText: {
+    marginBottom: 4,
   },
 });
