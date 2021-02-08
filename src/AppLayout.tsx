945b2dd779d04868f6c44f1a6f00d40c3a135023
meggsan <meggie.sanchez@gmail.com>
2021-02-08 18:45:29 -0400

    :sparkles: configuration with app icon and splash screen

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import RNBootSplash from 'react-native-bootsplash';
+
 import {DrawerNavigator} from './navigators/DrawerNavigator';
 
 export const AppLayout = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
+
   return <DrawerNavigator />;
 };
